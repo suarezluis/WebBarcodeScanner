03c80cb09a86ef02ad53d8f83064192954d09ee5
Luis Suarez <suarezluis@pop-os.localdomain>
2019-12-18 10:53:02 -0600

    Added debug square for reader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 Quagga.init(
   {
+    config: { locate: true },
     inputStream: {
       name: "Live",
       type: "LiveStream",
@@ -8,10 +9,10 @@ Quagga.init(
     decoder: {
       readers: ["code_128_reader"],
       debug: {
-        drawBoundingBox: true,
-        showFrequency: true,
-        drawScanline: true,
-        showPattern: true
+        drawBoundingBox: false,
+        showFrequency: false,
+        drawScanline: false,
+        showPattern: false
       }
     }
   },
@@ -24,7 +25,7 @@ Quagga.init(
     Quagga.start();
   }
 );
-
+let oldCode = "";
 Quagga.onDetected(data => {
   //   alert(data.codeResult.code);
   // console.log(data.codeResult.code);
@@ -33,8 +34,55 @@ Quagga.onDetected(data => {
   let regex = /^(?=.*[0-9])(?=.*[A-z])[0-9A-z-]{17}$/;
   let match = regex.test(barCode);
   if (match) {
+    var snd = new Audio();
+    snd.src = "../media/beep.mp3";
+    if (barCode != oldCode) {
+      snd.play();
+      oldCode = barCode;
+    }
+
     $(".result").html(data.codeResult.code);
-    $("#camera").html(data.codeResult.code);
-    Quagga.stop();
+    // $("#camera").html(""data.codeResult.code"");
+    // Quagga.stop();
+  }
+});
+
+Quagga.onProcessed(function(result) {
+  var drawingCtx = Quagga.canvas.ctx.overlay,
+    drawingCanvas = Quagga.canvas.dom.overlay;
+
+  if (result) {
+    if (result.boxes) {
+      drawingCtx.clearRect(
+        0,
+        0,
+        parseInt(drawingCanvas.getAttribute("width")),
+        parseInt(drawingCanvas.getAttribute("height"))
+      );
+      result.boxes
+        .filter(function(box) {
+          return box !== result.box;
+        })
+        .forEach(function(box) {
+          Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+            color: "green",
+            lineWidth: 2
+          });
+        });
+    }
+
+    if (result.box) {
+      Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+        color: "#00F",
+        lineWidth: 2
+      });
+    }
+
+    if (result.codeResult && result.codeResult.code) {
+      Quagga.ImageDebug.drawPath(result.line, { x: "x", y: "y" }, drawingCtx, {
+        color: "red",
+        lineWidth: 3
+      });
+    }
   }
 });
